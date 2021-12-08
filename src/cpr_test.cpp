0b35c185bbe7c34e6d3fce5fc1e954d57e598d3a
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2021-12-08 17:58:58 +0100

    cpr first test on svo

--- src/cpr_test.cpp
+++ src/cpr_test.cpp
@@ -2,20 +2,21 @@
  * @file cpr_test.cpp
  * @brief  testing CPR to download data from SVO
  * @version 0.1
- * 
+ *
  */
 #include <iostream>
 #include <cpr/cpr.h>
 
 /**
  * @brief main interface to SVO data requests
- * 
+ *
  * @param id              passband id
  * @return std::string    data content
  */
 std::string download_svo_filter(std::string id){
-    cpr::Response r = cpr::Get(cpr::Url{"http://svo2.cab.inta-csic.es/theory/fps/getdata.php?format=votable&id=Generic/Bessell_JHKLM.J"});
-                               // cpr::Parameters{{"format", "ascii"}, {"id", "Generic/Bessell_JHKLM.J"}});
+    cpr::Response r = cpr::Get(cpr::Url{"http://svo2.cab.inta-csic.es/theory/fps/fps.php"},
+                               cpr::Parameters{{"ID", id}});
+
     std::cout << "\n URL: " << r.url << std::endl;
     std::cout << "\n Content: \n" << r.text << std::endl;
     return r.text;
@@ -23,11 +24,5 @@ std::string download_svo_filter(std::string id){
 
 
 int main(){
-
-    // the following works
-    cpr::Response r = cpr::Get(cpr::Url{"http://svo2.cab.inta-csic.es/theory/fps/getdata.php?format=votable&id=Generic/Bessell_JHKLM.J"});
-
-    // the following generates a segfault
-   cpr::Parameters{{"format", "ascii"}, {"id", "Generic/Bessell_JHKLM.J"}}; 
-
+   std::cout << download_svo_filter("2MASS/2MASS.H") << "\n";
 }
