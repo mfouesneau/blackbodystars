985ecb312fd7a69ae38dd949c52a571cf68fd811
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2021-12-09 10:31:28 +0100

    use internals

--- src/cpr_test.cpp
+++ src/cpr_test.cpp
@@ -5,33 +5,10 @@
  *
  */
 #include <iostream>
-#include <cpr/cpr.h>
-#include "cphot/filter.hpp"
 #include "cphot/io.hpp"
-#include "cphot/votable.hpp"
-
-/**
- * @brief main interface to SVO data requests
- *
- * @param id              passband id
- * @return std::string    data content
- */
-std::string download_svo_filter(std::string id){
-    cpr::Response r = cpr::Get(cpr::Url{"http://svo2.cab.inta-csic.es/theory/fps/fps.php"},
-                               cpr::Parameters{{"ID", id}});
-
-    std::cout << "\n URL: " << r.url << std::endl;
-    return r.text;
-}
 
 
 int main(){
-    //const auto xml = download_svo_filter("GAIA/GAIA3.G");
-    const auto xml = download_svo_filter("2MASS/2MASS.H");
-    std::cout << xml << "\n";
-
-    votable::VOTable vot;
-    vot.from_content(xml);
-    auto filt = cphot::get_filter(vot);
+    cphot::Filter filt = cphot::download_svo_filter("2MASS/2MASS.H");
     filt.info();
 }
